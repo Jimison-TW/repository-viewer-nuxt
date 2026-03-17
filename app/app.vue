f6808a09d47e4bd8e6fc672a28ad32341b53939a
fish <>
2026-03-18 02:28:01 +0800

    新增RepoCard與RepoList元件

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,9 @@
 <template>
-  <div>
-    <AppHeader />
+  <div class="min-h-screen bg-slate-900">
+    <AppHeader title="Repository Viewer" />
+    <RepoList username="google" />
   </div>
 </template>
 
 <script setup lang="ts">
-const { repos, fetchRepos } = useListUserReposAPI('Google')
-
-onMounted(async () => {
-  console.log('測試開始：抓取第一頁資料')
-  if (repos.value.length === 0) {
-    await fetchRepos()
-  }
-  console.log('抓取結果：', repos.value)
-})
 </script>
