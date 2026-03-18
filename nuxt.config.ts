e30c9a95c1aa5c8561a514f714b9a74a694fef08
fish <>
2026-03-18 17:10:18 +0800

    新增gh-pages套件與輸出設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,18 @@ export default defineNuxtConfig({
         'Referrer-Policy': 'strict-origin-when-cross-origin',
       }
     }
+  },
+  app: {
+    baseURL: '/repository-viewer-nuxt/',
+    buildAssetsDir: 'assets', // 將底線改成普通資料夾名稱
+  },
+  // 建議加上這個，強制 Nuxt 進入靜態生成模式
+  ssr: false,
+  // 讓 Nitro 引擎生成的路徑更相容 GitHub Pages
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/']
+    }
   }
 })
